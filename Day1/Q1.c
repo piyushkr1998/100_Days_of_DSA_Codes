c14a166530bec1b8be19b9b026852f47c404de5d
Piyush Kumar <piyushkr6722@gmail.com>
2026-02-09 16:28:33 +0530

    First Code

---
+++ Day1/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, pos, x, i;
+
+    printf("Enter number of element :",n); 
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for(i = n; i >= pos; i--)
+        a[i] = a[i - 1];
+
+    a[pos - 1] = x;
+    n++;
+
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
