e2a4b25d573df2e2cc9a16f5e374e1b5ce8f7758
Piyush Kumar <piyushkr6722@gmail.com>
2026-02-16 11:26:08 +0530

    Day16

---
+++ Day16/Q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a[100], visited[100] = {0};
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+        for(int j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d:%d\n", a[i], count);
+    }
+
+    return 0;
+}
