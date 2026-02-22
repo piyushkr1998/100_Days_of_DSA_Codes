57a29954466fd8b2e5add60ba92d7bbb37658177
Piyush Kumar <piyushkr6722@gmail.com>
2026-02-22 14:04:26 +0530

    Day22

---
+++ Day22/Q1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, value;
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    printf("%d\n", countNodes(head));
+
+    return 0;
+}
