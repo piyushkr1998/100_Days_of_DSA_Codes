319ac41a6cb53da5852f542be578557e4d850b12
Piyush Kumar <piyushkr6722@gmail.com>
2026-03-15 21:46:47 +0530

    Day34

---
+++ Day35/Q1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *front = NULL;
+struct node *rear = NULL;
+
+void enqueue(int x)
+{
+    struct node *temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+
+    temp->data = x;
+    temp->next = NULL;
+
+    if(front == NULL)
+    {
+        front = rear = temp;
+    }
+    else
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+void display()
+{
+    struct node *temp = front;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int n, x;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
