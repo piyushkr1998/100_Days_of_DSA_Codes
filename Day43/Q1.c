52de4a05e58dd5a160ee6a27a1b5ca8ea0896156
Piyush Kumar <piyushkr6722@gmail.com>
2026-03-18 12:41:12 +0530

    Day43

---
+++ Day43/Q1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n, int i) {
+
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, N, 0);
+
+    inorder(root);
+
+    return 0;
+}
