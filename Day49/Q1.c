8f1cd5ea37cada10dd7d3d7e37b6ae5ad5252dac
Piyush Kumar <piyushkr6722@gmail.com>
2026-03-21 23:42:49 +0530

    Day49

---
+++ Day49/Q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int item) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+struct Node* insert(struct Node* node, int key) {
+    if (node == NULL) {
+        return newNode(key);
+    }
+
+    if (key < node->data) {
+        node->left = insert(node->left, key);
+    } else if (key > node->data) {
+        node->right = insert(node->right, key);
+    }
+    return node;
+}
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    
+    printf("Inorder traversal: ");
+    inorder(root);
+    
+    return 0;
+}
