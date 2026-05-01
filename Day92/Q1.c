90b6ecb62764017802432e587429de7c9df4f006
Piyush kumar <piyushkr6722@gmail.com>
2026-05-02 00:56:25 +0530

    Day92

---
+++ Day92/Q1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// Swap function
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Lomuto Partition
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];   // last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;  // pivot index
+}
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        // Recursively sort left and right
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+
+    // Input size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Call quick sort
+    quickSort(arr, 0, n - 1);
+
+    // Print sorted array
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
