c21ed0285f88b66f023b2f437f939a2a6b23ebb0
Piyush kumar <piyushkr6722@gmail.com>
2026-05-05 23:20:13 +0530

    Final Day Day 100

---
+++ Day100/Q1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+    int val;
+    int index;
+} Node;
+void merge(Node arr[], int left, int mid, int right, int count[]) {
+    int i = left, j = mid + 1, k = 0;
+    Node temp[right - left + 1];
+
+    int rightCount = 0; 
+
+    while (i <= mid && j <= right) {
+        if (arr[j].val < arr[i].val) {
+            temp[k++] = arr[j++];
+            rightCount++;  
+        } else {
+            count[arr[i].index] += rightCount;
+            temp[k++] = arr[i++];
+        }
+    }
+    while (i <= mid) {
+        count[arr[i].index] += rightCount;
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (i = left, k = 0; i <= right; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+void mergeSort(Node arr[], int left, int right, int count[]) {
+    if (left >= right) return;
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid, count);
+    mergeSort(arr, mid + 1, right, count);
+
+    merge(arr, left, mid, right, count);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int input[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &input[i]);
+
+    Node arr[n];
+    int count[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i].val = input[i];
+        arr[i].index = i;
+        count[i] = 0;
+    }
+
+    mergeSort(arr, 0, n - 1, count);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", count[i]);
+    }
+
+    return 0;
+}
